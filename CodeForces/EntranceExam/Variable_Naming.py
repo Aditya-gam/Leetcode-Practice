5af9e8612c666fbad72e68147a2162ada2da6ebd
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2024-01-11 17:30:56 -0800

    Completed Q2 Variable Naming

---
+++ CodeForces/EntranceExam/Variable_Naming.py
@@ -0,0 +1,48 @@
+# input
+# 7
+# 1 3 1 2 1 5 5
+
+# output
+# 1 3 2 4 5 10 15
+
+def variable_naming(var_names):
+    """
+    Generate variable names based on a given strategy.
+
+    Args:
+    - var_names (list): List of integers representing variables.
+
+    Returns:
+    - list: List of generated variable names.
+    """
+
+    # Create a set to store used variable names
+    mapping = set()
+    # Create a list to store the output variable names
+    output = []
+
+    # Iterate through the given variable names
+    for var in var_names:
+        i = 1
+        # Keep incrementing i until a unique variable name is found
+        while var * i in mapping:
+            i += 1
+
+        # Add the unique variable name to the set and the output list
+        mapping.add(var * i)
+        output.append(var * i)
+    
+    # Return the list of generated variable names
+    return output
+
+
+if __name__ == "__main__":
+    n = int(input())
+    var_names = list(map(int, input().split()))
+
+    print(*variable_naming(var_names))
+
+     
+    
+
+   
