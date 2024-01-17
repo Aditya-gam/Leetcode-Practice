2d988276020407d413a7c42132be7c0e660cc3d5
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2024-01-16 21:20:29 -0800

    Two Pointer Approach

---
+++ Easy/Move_Zeros.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        # Approach 1: Using Two Pointers
+        # Time Complexity: O(n)
+        # Space Complexity: O(1)
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i +=1
+
+        return nums
+    
