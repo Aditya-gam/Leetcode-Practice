28464be6ef2a78928a76141885aa9e8c03958e04
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2024-07-26 13:12:22 +0530

    Easy 1768. Merge Strings Alternately

---
+++ Easy/1768MergeStringsAlternately.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        max_len = max(len(word1), len(word2))
+        result = []
+        for i in range(max_len):
+            if i < len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
+
+        return "".join(result)
+
+    def mergeAlternately2(self, word1, word2):
+        min_len = min(len(word1), len(word2))
+        res = ''
+
+        for i in range(0, min_len):
+            res += word1[i]
+            res += word2[i]
+
+        res += word1[min_len:] + word2[min_len:]
+
+        return res
+    
+git commit -m "Easy 1768. Merge Strings Alternately"
