0031e2197b4be8469138ec550d0a83da003b2b2e
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2024-08-03 23:07:36 +0530

    Easy 121. Best Time to Buy and Sell Stock

--- Easy/0643MaximumAverageSubarray1.py
+++ Easy/0643MaximumAverageSubarray1.py
@@ -8,7 +8,7 @@ class Solution(object):
         # Calculate the sum of the first k elements
         current_sum = sum(nums[:k])
         max_sum = current_sum
-        
+
         # Slide the window across the rest of the elements
         for i in range(k, len(nums)):
             # Update the sum by subtracting the element that is sliding out and adding the new element
@@ -16,6 +16,6 @@ class Solution(object):
             # Update the max_sum if the current window's sum is greater
             if current_sum > max_sum:
                 max_sum = current_sum
-        
+
         # The maximum average is the maximum sum divided by k
         return max_sum / float(k)
